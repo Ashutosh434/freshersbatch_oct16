8a3d9076fea0e9e524e879a2229ce31a48c6dbbc
Ashutosh <83129217+Ashutosh434@users.noreply.github.com>
2021-11-21 16:49:29 +0530

    Delete SRA3/Assignment-3/Question3 directory

--- SRA3/Assignment-3/Question3/bankapp/BankAccountRepository.java
+++
@@ -1,6 +0,0 @@
-package com.bankapp;
-
-public interface BankAccountRepository {
-	double getBalance(long accountID);
-	double updateBalance(long accountID,double newBalance);
-}
--- SRA3/Assignment-3/Question3/bankapp/BankAccountService.java
+++
@@ -1,7 +0,0 @@
-package com.bankapp;
-
-public interface BankAccountService {
-	double widthdraw(long accountId,double balance);
-	double deposit(long accountId,double balance);
-	double getBalance(long accountId);
-}
