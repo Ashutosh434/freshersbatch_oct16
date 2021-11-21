bdab45e28645dee234e8f4dba9efe056bcd85ecd
Ashutosh <83129217+Ashutosh434@users.noreply.github.com>
2021-11-21 16:49:46 +0530

    Delete SRA5/ques5 directory

--- SRA5/ques5/src/main/java/com/rest/exerciseems/modelrepository/Repository.java
+++
@@ -1,9 +0,0 @@
-package com.rest.exerciseems.modelrepository;
-
-import com.rest.exerciseems.model.Employee;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-public interface Repository extends MongoRepository<Employee,String> {
-
-}
