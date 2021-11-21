38849a23635d55cda6accec64bfc23144d67528c
Ashutosh <83129217+Ashutosh434@users.noreply.github.com>
2021-11-21 16:50:09 +0530

    Delete SRA7/ques 7 directory

--- SRA7/ques 7/src/main/java/com/exercise/customer/modelreposistory/Repository.java
+++
@@ -1,8 +0,0 @@
-package com.exercise.customer.modelreposistory;
-
-import com.exercise.customer.model.Customer;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface Repository extends MongoRepository<Customer,String> {
-
-}
--- SRA7/ques 7/src/main/java/com/exercise/order/modelreposistory/Repository.java
+++
@@ -1,8 +0,0 @@
-package com.exercise.order.modelreposistory;
-
-import com.exercise.order.model.Order;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface Repository extends MongoRepository<Order,String> {
-
-}
